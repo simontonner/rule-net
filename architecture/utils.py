56f66a3e767e12afba943facb0bce5eb7b89a498
Simon <simon.tonner@outlook.com>
2025-09-14 17:45:54 +0200

    Simplified printing logic

--- architecture/utils.py
+++ architecture/utils.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 import numpy as np
 
 
@@ -29,3 +30,35 @@ def truth_table_indices(bit_patterns: np.ndarray) -> np.ndarray[np.int64]:
     descending_indices = max_index - ascending_indices
 
     return descending_indices.astype(np.int64)
+
+
+def describe_architecture(net) -> Dict[str, Any]:
+    """
+    Describes the architecture of a fitted "DeepBinaryClassifier" network as a nested dictionary.
+
+    :param net: The fitted network to describe
+    :return: A dictionary representing the architecture
+    """
+    if not getattr(net, "layers", None):
+        raise RuntimeError("Network not fitted. Nothing to describe.")
+
+    num_layers = len(net.layers)
+
+    input_nodes = [{"name": n} for n in net.node_names[0]]
+    layers = [{"name": "INPUT", "nodes": input_nodes}]
+
+    for layer_idx in range(num_layers):
+        prev_layer_node_names = net.node_names[layer_idx]
+        layer_node_names = net.node_names[layer_idx + 1]
+        layer_backlinks = net.backlinks[layer_idx + 1]
+
+        layer_nodes = []
+        for node_idx, node in enumerate(net.layers[layer_idx]):
+            prev_node_names = [prev_layer_node_names[bl] for bl in layer_backlinks[node_idx]]
+
+            layer_nodes.append({"name": layer_node_names[node_idx], "backlinks": prev_node_names, "metadata": node.get_metadata()})
+
+        layer_name = "OUTPUT" if layer_idx == num_layers - 1 else f"L{layer_idx+1}"
+        layers.append({"name": layer_name, "nodes": layer_nodes})
+
+    return {"layers": layers}
